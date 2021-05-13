14b48197a35ec641438e6d42e5db272067bf14d9
Apryl Smith <aprylsmith@Ryles-iMac.attlocal.net>
2021-05-13 13:49:42 -0400

    add data pull from axios and card component

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,14 @@
+import axios from 'axios';
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const result = axios.get("https://api.github.com/users/GidgetPINK")
+//console.log(result) - result is a promise: an object representing the result of a computation
+
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,6 +22,59 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+result.then((data) => {
+  //create the function here, within the .then
+  const gitCardMaker = (obj) => {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add("card");
+    document.querySelector(".cards").appendChild(cardContainer);
+
+    const cardImg = document.createElement('img');
+    cardImg.src = obj.avatar_url;
+    document.querySelector(".card").appendChild(cardImg);
+
+    const cardDetails = document.createElement('div');
+    cardDetails.classList.add("card-info");
+    document.querySelector(".card").appendChild(cardDetails);
+
+    const heading = document.createElement('h3');
+    heading.classList.add("name");
+    heading.innerHTML = obj.name;
+    document.querySelector(".card-info").appendChild(heading);
+    
+
+    const para = document.createElement('p');
+    para.classList.add("username");
+    para.innerHTML = obj.login;
+    document.querySelector(".card-info").appendChild(para);
+
+    const para1 = document.createElement('p');
+    para1.innerHTML = "Location:", obj.location;
+    document.querySelector(".card-info").appendChild(para1);
+
+    const para2 = document.createElement('p');
+    para2.innerHTML = "Profile:";
+    para2.href = obj.html_url;
+    document.querySelector(".card-info").appendChild(para2);
+
+    const para3 = document.createElement('p');
+    para3.innerHTML = "Followers:", obj.followers;
+    document.querySelector(".card-info").appendChild(para3);
+
+    const para4 = document.createElement('p');
+    para4.innerHTML = "Following:", obj.following;
+    document.querySelector(".card-info").appendChild(para4);
+
+    const para5 = document.createElement('p');
+    para5.innerHTML = "Bio:", obj.bio;
+    document.querySelector(".card-info").appendChild(para5);
+    
+    return cardContainer
+  }
+  console.log(gitCardMaker(result));
+});
+
+ 
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
